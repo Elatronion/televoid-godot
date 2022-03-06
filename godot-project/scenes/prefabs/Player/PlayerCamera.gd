4678193d883f42a0abe7820feb99bf76cf7d7221
Alexis Millette <katsuro.userg@gmail.com>
2022-03-05 20:59:47 -0500

    fix camera offset when playing imv
    Camera offset is reset to zero when playing imv, also not updated while doing so.
    The camera offset is lessened as requested.

--- godot-project/scenes/prefabs/Player/PlayerCamera.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-
-
-func _process(delta):
-	offset_h = lerp(-0.5, 0.5, get_viewport().get_mouse_position().x / get_viewport_rect().size.x)
-	offset_v = lerp(-0.5, 0.5, get_viewport().get_mouse_position().y / get_viewport_rect().size.y)
