e439f5f6894265db065e58705be94bd183f82e52
Ezra Hradecky <ezra@duck.com>
2022-03-18 08:52:47 -0400

    tweak bowling gameplay

--- godot-project/scenes/prefabs/Bowling/Minigame/BowlingMinigame.gd
+++ godot-project/scenes/prefabs/Bowling/Minigame/BowlingMinigame.gd
@@ -16,6 +16,7 @@ var throw_direction = Vector3(-1, 0, 0)
 
 enum BowlingMinigameState {
 	AIM,
+	YAW,
 	ROLLING,
 	HIT,
 	SCREEN,
@@ -35,32 +36,39 @@ func throw_ball():
 	$BowlingBalls.add_child(bowling_ball_instance)
 	bowling_ball_instance.apply_impulse(Vector3(0, 0, 0), throw_direction * throw_power)
 	current_state = BowlingMinigameState.ROLLING
+	
+	throw_arrow.get_node("ThrowArrowPivot").look_at(throw_arrow.global_transform.origin + Vector3(-1, 0, 0), Vector3.UP)
 
 var hit_timer_active = false
 var screen_timer_active = false
 var t = 0
 func _process(delta):
 	t += delta
-	throw_arrow.transform.origin.z = cos(t) * 1.75
 	
 	if current_state == BowlingMinigameState.AIM:
+		throw_arrow.transform.origin.z = cos(t) * 1.75
 		minigame_camera.global_transform.origin = throw_arrow_camera_position.global_transform.origin
 		minigame_camera.look_at(Vector3(-28.481, 0, -1), Vector3.UP)
 		minigame_camera.fov = 70
 		if Input.is_action_just_pressed("interact"):
+			current_state = BowlingMinigameState.YAW
+	elif current_state == BowlingMinigameState.YAW:
+		throw_direction.z = sin(t*(82/60)) * 0.5
+		throw_arrow.get_node("ThrowArrowPivot").look_at(throw_arrow.global_transform.origin + throw_direction, Vector3.UP)
+		if Input.is_action_just_pressed("interact"):
 			throw_ball()
-	if current_state == BowlingMinigameState.ROLLING:
+	elif current_state == BowlingMinigameState.ROLLING:
 		if bowling_balls.get_children().size() > 0:
 			minigame_camera.global_transform.origin = view_camera_position.global_transform.origin
 			minigame_camera.look_at(bowling_balls.get_children()[0].global_transform.origin, Vector3.UP)
 			minigame_camera.fov = minigame_camera.fov - (minigame_camera.fov - 10) * 0.25 * delta
 		else:
 			current_state = BowlingMinigameState.HIT
-	if current_state == BowlingMinigameState.HIT:
+	elif current_state == BowlingMinigameState.HIT:
 		if not hit_timer_active:
 			hit_timer.start()
 			hit_timer_active = true
-	if current_state == BowlingMinigameState.SCREEN:
+	elif current_state == BowlingMinigameState.SCREEN:
 		if not screen_timer_active:
 			screen_timer.start()
 			screen_timer_active = true
